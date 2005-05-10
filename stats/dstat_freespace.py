6e1b2e1317e9962ebf57901308d6a8715801790e
Dag Wieers <dag@wieers.com>
2005-05-10 02:44:25 +0000

    Small updates

--- stats/dstat_freespace.py
+++ stats/dstat_freespace.py
@@ -8,7 +8,7 @@ class dstat_freespace(dstat.dstat):
 		self.open('/etc/mtab')
 		self.vars = self.vars()
 		self.name = ['/' + os.path.basename(name) for name in self.vars]
-		self.nick = ('avail', 'free')
+		self.nick = ('free', 'used')
 		self.init(self.vars, 2)
 
 	def vars(self):
@@ -20,12 +20,14 @@ class dstat_freespace(dstat.dstat):
 				if len(l) < 6: continue
 				if l[0] in ('none', 'usbfs', 'sunrpc'): continue
 				name = l[1] 
+				res = os.statvfs(name)
+				if res[8] == 1 or res[8] == 15: continue ### Leave out loop/iso images
 				ret.append(name)
 		return ret
 
 	def extract(self):
 		for name in self.vars:
 			res = os.statvfs(name)
-			self.val[name] = ( long(res[3]) * long(res[0]), long(res[2]) * long(res[0]) )
+			self.val[name] = ( long(res.f_bavail) * long(res.f_frsize), (long(res.f_blocks) - long(res.f_bavail)) * long(res.f_frsize) )
 
 # vim:ts=4:sw=4
