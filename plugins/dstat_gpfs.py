90395a608fdd302adb14c54b65d189cffb8a9718
Dag Wieers <dag@wieers.com>
2005-05-24 17:25:01 +0000

    More necessary GPFS improvements

--- plugins/dstat_gpfs.py
+++ plugins/dstat_gpfs.py
@@ -1,18 +1,20 @@
-global string, popen2
-import string, popen2
+global string, select
+import string, select
 
 class dstat_gpfs(dstat):
 	def __init__(self):
 		self.name = 'gpfs i/o'
 		self.format = ('f', 5, 1024)
-		self.vars = ('read', 'write')
-		self.nick = self.vars
+		self.vars = ('_br_', '_bw_')
+		self.nick = ('read', 'write')
 		self.init(self.vars, 1)
 
 	def check(self): 
 		if os.access('/usr/lpp/mmfs/bin/mmpmon', os.X_OK):
 			try:
-				self.stdout, self.stdin, self.stderr = popen2.popen3('/usr/lpp/mmfs/bin/mmpmon', 0)
+				self.stdin, self.stdout, self.stderr = dpopen('/usr/lpp/mmfs/bin/mmpmon -p')
+				self.stdin.write('reset\n')
+				readpipe(self.stdout)
 			except IOError:
 				raise Exception, 'Module can not interface with gpfs mmpmon binary'
 			return True
@@ -21,22 +23,23 @@ class dstat_gpfs(dstat):
 
 	def extract(self):
 		try:
-			self.stdout.flush()
-			self.stdin.write('io_s\n'); size = 290
-#			self.stdin.write('fs_io_s\n'); size = 370
-			for line in self.stdout.read(size).split('\n'):
+			self.stdin.write('io_s\n')
+#			readpipe(self.stderr)
+			for line in readpipe(self.stdout):
+				if not line: continue
 				l = line.split()
-				if len(l) != 3: continue
-#				print l
-				name = l[0:2]
-				if name == ['bytes','read:']:
-					self.cn2['read'] = long(l[2])
-				elif name == ['bytes','written:']:
-					self.cn2['write'] = long(l[2])
+				for name in self.vars:
+					self.cn2[name] = long(l[l.index(name)+1])
 		except IOError, e:
-			print e
+			for name in self.vars: self.val[name] = -1
+			print 'dstat_gpfs: lost pipe to mmpmon,', e
+		except Exception, e:
+			for name in self.vars: self.val[name] = -1
+			print 'dstat_gpfs: exception', e
+
 		for name in self.vars:
 			self.val[name] = (self.cn2[name] - self.cn1[name]) * 1.0 / tick
+
 		if step == op.delay:
 			self.cn1.update(self.cn2)
 
--- plugins/dstat_gpfsop.py
+++ plugins/dstat_gpfsop.py
@@ -1,18 +1,20 @@
-global string, popen2
-import string, popen2
+global string, select
+import string, select
 
 class dstat_gpfsop(dstat):
 	def __init__(self):
 		self.name = 'gpfs operations'
 		self.format = ('d', 4, 1000)
-		self.vars = ('opens', 'closes', 'reads', 'writes', 'readdir', 'inodup')
+		self.vars = ('_oc_', '_cc_', '_rdc_', '_wc_', '_dir_', '_iu_')
 		self.nick = ('open', 'clos', 'read', 'writ', 'rdir', 'inod')
 		self.init(self.vars, 1)
 
 	def check(self): 
 		if os.access('/usr/lpp/mmfs/bin/mmpmon', os.X_OK):
 			try:
-				self.stdout, self.stdin, self.stderr = popen2.popen3('/usr/lpp/mmfs/bin/mmpmon', 0)
+				self.stdin, self.stdout, self.stderr = dpopen('/usr/lpp/mmfs/bin/mmpmon -p')
+				self.stdin.write('reset\n')
+				readpipe(self.stdout)
 			except IOError:
 				raise Exception, 'Module can not interface with gpfs mmpmon binary'
 			return True
@@ -21,25 +23,23 @@ class dstat_gpfsop(dstat):
 
 	def extract(self):
 		try:
-			self.stdout.flush()
-			self.stdin.write('io_s\n'); size = 290
-#			self.stdin.write('fs_io_s\n'); size = 370
-			for line in self.stdout.read(size).split('\n'):
+			self.stdin.write('io_s\n')
+#			readpipe(self.stderr)
+			for line in readpipe(self.stdout):
+				if not line: continue
 				l = line.split()
-				if len(l) < 2: continue
-				if len(l) > 3: continue
-				name = l[0].split(':')[0]
-				if len(l) > 3:  continue
-				if name in self.vars:
-#					print line
-					self.cn2[name] = long(l[1])
-				elif len(l) >= 3 and name == 'inode':
-#					print line
-					self.cn2['inodup'] = long(l[2])
+				for name in self.vars:
+					self.cn2[name] = long(l[l.index(name)+1])
 		except IOError, e:
-			print e
+			for name in self.vars: self.val[name] = -1
+			print 'dstat_gpfs: lost pipe to mmpmon,', e
+		except Exception, e:
+			for name in self.vars: self.val[name] = -1
+			print 'dstat_gpfs: exception', e
+
 		for name in self.vars:
 			self.val[name] = (self.cn2[name] - self.cn1[name]) * 1.0 / tick
+
 		if step == op.delay:
 			self.cn1.update(self.cn2)
 
