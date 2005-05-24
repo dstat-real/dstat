0617b095f7c3421e85fd4b97cf651dd6e15a82cb
Dag Wieers <dag@wieers.com>
2005-05-24 07:00:22 +0000

    import GPFS plugins

---
+++ plugins/dstat_gpfs.py
@@ -0,0 +1,44 @@
+global string, popen2
+import string, popen2
+
+class dstat_gpfs(dstat):
+	def __init__(self):
+		self.name = 'gpfs i/o'
+		self.format = ('f', 5, 1024)
+		self.vars = ('read', 'write')
+		self.nick = self.vars
+		self.init(self.vars, 1)
+
+	def check(self): 
+		if os.access('/usr/lpp/mmfs/bin/mmpmon', os.X_OK):
+			try:
+				self.stdout, self.stdin, self.stderr = popen2.popen3('/usr/lpp/mmfs/bin/mmpmon', 0)
+#				self.stdin.write('fs_io_s\n')
+				self.stdin.write('io_s\n')
+			except IOError:
+				raise Exception, 'Module can not interface with gpfs mmpmon binary'
+			return True
+		raise Exception, 'Module needs gpfs mmpmon binary'
+		return false
+
+	def extract(self):
+		try:
+			self.stdout.flush()
+			for line in self.stdout.read(371).split('\n'):
+				l = line.split()
+				if len(l) != 3: continue
+#				print l
+				name = l[0:2]
+				if name == ['bytes','read:']:
+					self.cn2['read'] = long(l[2])
+				elif name == ['bytes','written:']:
+					self.cn2['write'] = long(l[2])
+			self.stdin.write('fs_io_s\n')
+		except IOError, e:
+			print e
+		for name in self.vars:
+			self.val[name] = (self.cn2[name] - self.cn1[name]) * 1.0 / tick
+		if step == op.delay:
+			self.cn1.update(self.cn2)
+
+# vim:ts=4:sw=4
---
+++ plugins/dstat_gpfsop.py
@@ -0,0 +1,44 @@
+global string, popen2
+import string, popen2
+
+class dstat_gpfsop(dstat):
+	def __init__(self):
+		self.name = 'gpfs operations'
+		self.format = ('d', 4, 1000)
+		self.vars = ('opens', 'closes', 'reads', 'writes', 'readdir', 'inodup')
+		self.nick = ('open', 'clos', 'read', 'writ', 'rdir', 'inod')
+		self.init(self.vars, 1)
+
+	def check(self): 
+		if os.access('/usr/lpp/mmfs/bin/mmpmon', os.X_OK):
+			try:
+				self.stdout, self.stdin, self.stderr = popen2.popen3('/usr/lpp/mmfs/bin/mmpmon', 0)
+#				self.stdin.write('fs_io_s\n')
+				self.stdin.write('io_s\n')
+			except IOError:
+				raise Exception, 'Module can not interface with gpfs mmpmon binary'
+			return True
+		raise Exception, 'Module needs gpfs mmpmon binary'
+		return false
+
+	def extract(self):
+		try:
+			self.stdout.flush()
+			for line in self.stdout.read(371).split('\n'):
+				l = line.split()
+				if len(l) < 2: continue
+				if len(l) > 3: continue
+				name = l[0].split(':')[0]
+				if name in self.vars:
+					self.cn2[name] = long(l[1])
+				elif len(l) >= 3 and name == 'inode':
+					self.cn2['inodup'] = long(l[2])
+			self.stdin.write('fs_io_s\n')
+		except IOError, e:
+			print e
+		for name in self.vars:
+			self.val[name] = (self.cn2[name] - self.cn1[name]) * 1.0 / tick
+		if step == op.delay:
+			self.cn1.update(self.cn2)
+
+# vim:ts=4:sw=4
