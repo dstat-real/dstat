9d2e2733ac83a295050df3131b55bf63dede6678
Dag Wieers <dag@wieers.com>
2007-12-05 18:35:37 +0000

    Fix for cpufreq scaling and docs.

--- plugins/dstat_cpufreq.py
+++ plugins/dstat_cpufreq.py
@@ -20,21 +20,21 @@ class dstat_cpufreq(dstat):
     def check(self): 
         if self.vars:
             for cpu in self.vars:
-                if not os.access('/sys/devices/system/cpu/'+cpu+'/cpufreq/cpuinfo_cur_freq', os.R_OK):
+                if not os.access('/sys/devices/system/cpu/'+cpu+'/cpufreq/scaling_cur_freq', os.R_OK):
                     raise Exception, 'Cannot access acpi cpu frequency information'
             return True
         raise Exception, 'No statistics found'
 
     def extract(self):
         for cpu in self.vars:
-            for line in dopen('/sys/devices/system/cpu/'+cpu+'/cpufreq/cpuinfo_max_freq').readlines():
+            for line in dopen('/sys/devices/system/cpu/'+cpu+'/cpufreq/scaling_max_freq').readlines():
                 l = string.split(line)
                 max = int(l[0])
-            for line in dopen('/sys/devices/system/cpu/'+cpu+'/cpufreq/cpuinfo_cur_freq').readlines():
+            for line in dopen('/sys/devices/system/cpu/'+cpu+'/cpufreq/scaling_cur_freq').readlines():
                 l = string.split(line)
                 cur = int(l[0])
             ### Need to close because of bug in sysfs (?)
-            dclose('/sys/devices/system/cpu/'+cpu+'/cpufreq/cpuinfo_cur_freq')
+            dclose('/sys/devices/system/cpu/'+cpu+'/cpufreq/scaling_cur_freq')
             self.val[cpu] = cur * 100.0 / max
 
 # vim:ts=4:sw=4:et
