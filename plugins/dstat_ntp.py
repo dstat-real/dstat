66c57f5f6ee5f5e8481eb04f72bc81a7abe9a636
Dag Wieers <dag@wieers.com>
2009-02-17 20:08:05 +0000

    Improvements to dstat_ntp.

--- plugins/dstat_ntp.py
+++ plugins/dstat_ntp.py
@@ -3,12 +3,19 @@
 ###
 ### Authority: dag@wieers.com
 
+### Beware that this dstat plugin typically takes a lot longer to run than
+### system plugins and for that reason it is important to use an NTP server
+### located nearby as well as make sure that it does not impact your other
+### counters too much.
+
 global socket
 import socket
+
 global struct
 import struct
 
 ### FIXME: Implement millisecond granularity as well
+### FIXME: Interrupts socket if data is overdue (more than 250ms ?)
 
 class dstat_ntp(dstat):
     def __init__(self):
@@ -19,36 +26,39 @@ class dstat_ntp(dstat):
         self.nick = ('date/time',)
         self.vars = ('time',)
         self.epoch = 2208988800L
+#        socket.setdefaulttimeout(0.25)
         self.socket = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+        self.socket.settimeout(0.25)
         self.init(self.vars, 1)
 
     def gettime(self):
-        self.socket.sendto( '\x1b' + 47 * '\0', ( self.ntpserver, 123 ))
-        data, address = self.socket.recvfrom( 1024 )
-
-        if data:
-            return (struct.unpack( '!12I', data )[10] - self.epoch)
-        else:
+        try:
+            self.socket.sendto( '\x1b' + 47 * '\0', ( self.ntpserver, 123 ))
+            data, address = self.socket.recvfrom(1024)
+        except socket.timeout:
             return 0
+        return struct.unpack( '!12I', data )[10] - self.epoch
 
     def check(self):
         try:
-            self.gettime()
-        except gaierror:
+            t = self.gettime()
+        except socket.gaierror:
             raise Exception, 'Failed to connect to NTP server.'
-        except error:
+        except socket.error:
             raise Exception, 'Error connecting to NTP server.'
         return True
 
     def extract(self):
-        starttime = self.gettime()
-        self.val['time'] = time.strftime(self.timefmt, time.localtime(starttime))
+        self.val['time'] = self.gettime()
 
     def show(self):
         if step == op.delay:
             color = 'silver'
         else:
             color = 'gray'
-        return ansi[color] + self.val['time']
+        if self.val['time']:
+            return ansi[color] + time.strftime(self.timefmt, time.localtime(self.val['time']))
+        else:
+            return ansi['white'] + ansi['redbg'] + '-'.rjust(self.format[1]-1) + ' ' + ansi['default']
 
 # vim:ts=4:sw=4:et
