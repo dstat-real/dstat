c4482e98ac58bb5c7909b3a8cb1a030fb48eff61
Dag Wieers <dag@wieers.com>
2010-07-01 12:37:46 +0000

    Added external dstat_md_dstat plugin to show the resync progress of SWRAID (Bert de Bruijn)

---
+++ plugins/dstat_md_status.py
@@ -0,0 +1,39 @@
+### Author: Bert de Bruijn <bert+dstat@debruijn.be>
+
+class dstat_plugin(dstat):
+    """
+    Recovery state of software RAID rebuild.
+
+    Prints completed recovery percentage and rebuild speed
+    of the md device that is actively being recovered or
+    resynced.
+    If no devices are being rebuilt, it displays 100%, 0B
+
+    Known issues: if multiple md devices are being rebuilt
+    at the same time, it only displays the last one.
+    """
+
+    def __init__(self):
+        self.name = 'raid'
+        self.type = 's'
+        self.scale = 0
+        self.nick = ('pct speed', )
+        self.width = 9
+        self.vars = ('text', )
+        self.open('/proc/mdstat')
+
+    def check(self):
+        if not os.path.exists('/proc/mdstat'):
+            raise Exception, 'Needs kernel md support'
+
+    def extract(self):
+        pct = 100
+        speed = 0
+        for l in self.splitlines():
+            if len(l) < 2: continue
+            if l[1] in ('recovery', 'resync'):
+                pct = int(l[3][0:2].strip('.%'))
+                speed = int(l[6].strip('sped=K/sc')) * 1024
+        self.val['text'] = '%s %s' % (cprint(pct, 'p', 3, 34), cprint(speed, 'd', 5, 1024))
+
+# vim:ts=4:sw=4:et
