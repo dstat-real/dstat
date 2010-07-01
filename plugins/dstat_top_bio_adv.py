d83b72cec76bd4332430ef230cccb1161cd86ef4
Dag Wieers <dag@wieers.com>
2010-07-01 12:37:03 +0000

    Added kernel config option to error message.

--- plugins/dstat_top_bio_adv.py
+++ plugins/dstat_top_bio_adv.py
@@ -14,7 +14,7 @@ class dstat_plugin(dstat):
 
     def check(self):
         if not os.access('/proc/self/io', os.R_OK):
-            raise Exception, 'Kernel has no per-process I/O accounting, use at least 2.6.20'
+            raise Exception, 'Kernel has no per-process I/O accounting [CONFIG_TASK_IO_ACCOUNTING], use at least 2.6.20'
         return True
 
     def extract(self):
