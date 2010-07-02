8bad3722fd82a7ba501ff11538602ee9bb220a13
Dag Wieers <dag@wieers.com>
2010-07-02 14:03:52 +0000

    - Changed color of 100% to white to make it stand out (Bert de Bruijn)

--- plugins/dstat_md_status.py
+++ plugins/dstat_md_status.py
@@ -27,13 +27,20 @@ class dstat_plugin(dstat):
             raise Exception, 'Needs kernel md support'
 
     def extract(self):
-        pct = 100
+        pct = 0
         speed = 0
+        nr = 0
         for l in self.splitlines():
             if len(l) < 2: continue
             if l[1] in ('recovery', 'resync'):
-                pct = int(l[3][0:2].strip('.%'))
-                speed = int(l[6].strip('sped=K/sc')) * 1024
+                nr += 1
+                pct += int(l[3][0:2].strip('.%'))
+                speed += int(l[6].strip('sped=K/sc')) * 1024
+        if nr:
+            speed = speed / nr
+            pct = pct / nr
+        else:
+            pct = 100
         self.val['text'] = '%s %s' % (cprint(pct, 'p', 3, 34), cprint(speed, 'd', 5, 1024))
 
 # vim:ts=4:sw=4:et
