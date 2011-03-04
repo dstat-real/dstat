931f47be5b6cf9c805496e4425a2c159da359989
Dag Wieers <dag@wieers.com>
2011-03-04 15:25:40 +0000

    Allow more variables (self.vars) than nicknames (self.nick) to simplify plugins

--- plugins/dstat_mem_adv.py
+++ plugins/dstat_mem_adv.py
@@ -10,13 +10,13 @@ class dstat_plugin(dstat):
     def __init__(self):
         self.name = 'advanced memory usage'
         self.nick = ('used', 'buff', 'cach', 'free', 'dirty')
-        self.vars = ('MemUsed', 'Buffers', 'Cached', 'MemFree', 'Dirty')
+        self.vars = ('MemUsed', 'Buffers', 'Cached', 'MemFree', 'Dirty', 'MemTotal')
         self.open('/proc/meminfo')
 
     def extract(self):
         for l in self.splitlines():
             if len(l) < 2: continue
             name = l[0].split(':')[0]
-            if name in self.vars + ('MemTotal', ):
+            if name in self.vars:
                 self.val[name] = long(l[1]) * 1024.0
         self.val['MemUsed'] = self.val['MemTotal'] - self.val['MemFree'] - self.val['Buffers'] - self.val['Cached']
