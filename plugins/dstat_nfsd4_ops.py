7d0f64217eaf3571423156a74b322653cc2ca7ce
Adam Michel <elfurbe@furbism.com>
2013-04-24 16:26:12 -0700

    Modified default metrics to show values that actually change

--- plugins/dstat_nfsd4_ops.py
+++ plugins/dstat_nfsd4_ops.py
@@ -5,34 +5,34 @@ class dstat_plugin(dstat):
     def __init__(self):
         self.name = 'nfs4 server'
         # this vars/nick pair is the ones I considered relevant. Any set of the full list would work.
-        #self.vars = ('read', 'write', 'readdir', 'commit', 'getattr', 'getfh', 'putfh')
-        #self.nick = ('read', 'writ', 'rdir', 'cmmt', 'gatr','gfh','pfh')
+        self.vars = ('read','write','readdir','getattr','setattr','commit','getfh','putfh',
+                'savefh','restorefh','open','open_conf','close','access','lookup','remove')
+        self.nick = ('read', 'writ', 'rdir', 'gatr','satr','cmmt','gfh','pfh','sfh','rfh',
+                'open','opnc','clse','accs','lkup','rem')
         # this is every possible variable for NFSv4 if you're into that
-        self.vars4 = ('op0-unused', 'op1-unused', 'op2-future' , 'access',
-                'close', 'commit', 'create', 'delegpurge', 'delegreturn', 'getattr', 'getfh',
-                'link', 'lock', 'lockt', 'locku', 'lookup', 'lookup_root', 'nverify', 'open',
-                'openattr', 'open_conf', 'open_dgrd','putfh', 'putpubfh', 'putrootfh',
-                'read', 'readdir', 'readlink', 'remove', 'rename','renew', 'restorefh',
-                'savefh', 'secinfo', 'setattr', 'setcltid', 'setcltidconf', 'verify', 'write',
-                'rellockowner')
+        #self.vars4 = ('op0-unused', 'op1-unused', 'op2-future' , 'access',
+        #        'close', 'commit', 'create', 'delegpurge', 'delegreturn', 'getattr', 'getfh',
+        #        'link', 'lock', 'lockt', 'locku', 'lookup', 'lookup_root', 'nverify', 'open',
+        #        'openattr', 'open_conf', 'open_dgrd','putfh', 'putpubfh', 'putrootfh',
+        #        'read', 'readdir', 'readlink', 'remove', 'rename','renew', 'restorefh',
+        #        'savefh', 'secinfo', 'setattr', 'setcltid', 'setcltidconf', 'verify', 'write',
+        #        'rellockowner')
         # I separated the NFSv41 ops cause you know, completeness.
-        self.vars41 = ('bc_ctl', 'bind_conn', 'exchange_id', 'create_ses',
-                'destroy_ses', 'free_stateid', 'getdirdeleg', 'getdevinfo', 'getdevlist',
-                'layoutcommit', 'layoutget', 'layoutreturn', 'secinfononam', 'sequence',
-                'set_ssv', 'test_stateid', 'want_deleg', 'destroy_clid', 'reclaim_comp')
+        #self.vars41 = ('bc_ctl', 'bind_conn', 'exchange_id', 'create_ses',
+        #        'destroy_ses', 'free_stateid', 'getdirdeleg', 'getdevinfo', 'getdevlist',
+        #        'layoutcommit', 'layoutget', 'layoutreturn', 'secinfononam', 'sequence',
+        #        'set_ssv', 'test_stateid', 'want_deleg', 'destroy_clid', 'reclaim_comp')
         # Just catin' the tuples together to make the full list.
         #self.vars = self.vars4 + self.vars41
-        self.vars = self.vars4
         # these are terrible shortnames for every possible variable
-        self.nick4 = ('unsd','unsd','unsd','accs','clse','comm','crt','delp','delr','gatr','gfh',
-                'link','lock','lckt','lcku','lkup','lkpr','nver','open','opna','opnc','opnd',
-                'pfh','ppfh','prfh','read','rdir','rlnk','rmv','ren','rnw','rfh','sfh','snfo',
-                'satr','scid','scic','ver','wrt','rlko')
-        self.nick41 = ('bctl','bcon','eid','cses','dses','fsid',
-                'gdd','gdi','gdl','lcmt','lget','lrtn','sinn','seq','sets','tsts','wdel','dcid',
-                'rcmp')
+        #self.nick4 = ('unsd','unsd','unsd','accs','clse','comm','crt','delp','delr','gatr','gfh',
+        #        'link','lock','lckt','lcku','lkup','lkpr','nver','open','opna','opnc','opnd',
+        #        'pfh','ppfh','prfh','read','rdir','rlnk','rmv','ren','rnw','rfh','sfh','snfo',
+        #        'satr','scid','scic','ver','wrt','rlko')
+        #self.nick41 = ('bctl','bcon','eid','cses','dses','fsid',
+        #        'gdd','gdi','gdl','lcmt','lget','lrtn','sinn','seq','sets','tsts','wdel','dcid',
+        #        'rcmp')
         #self.nick = self.nick4 + self.nick41
-        self.nick = self.nick4
         self.type = 'd'
         self.width = 5
         self.scale = 1000
